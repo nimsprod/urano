b9078ec334de9075b5c8c2f414b1582427fe4308
Jose Saez <jsaez@foreworth.com>
2021-03-01 10:51:07 +0100

    added crazy reader

---
+++ Urano/Implementations/Crazy/CrazyUranoStringReader.cs
@@ -0,0 +1,11 @@
+﻿namespace Urano.Implementations.Crazy
+{
+    public class CrazyUranoStringReader : IStringReader
+    {
+        public string ReadNumber(int number)
+        {
+            var rand = new Random();
+            return rand.Next(100).ToString();
+        }
+    }
+}
--- Urano/Program.cs
+++ Urano/Program.cs
@@ -1,5 +1,7 @@
-﻿using Urano.Implementations.Standard;
+﻿using Urano.Implementations.Crazy;
+using Urano.Implementations.Standard;
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine(new UranoStringReader().ReadNumber(5));
+Console.WriteLine(new CrazyUranoStringReader().ReadNumber(5));
 
